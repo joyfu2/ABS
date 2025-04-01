b5a2f46119b0d95ff9d6881385e3eadc3545e06d
Joy Fu <JoyFu@Joys-MBP-3.lan>
2025-03-31 21:42:32 -0400

    Update map styles to completely hide all POI elements

--- script.js
+++ script.js
@@ -22,38 +22,47 @@ function initializeMap() {
         styles: [
             {
                 featureType: 'poi',
+                elementType: 'all',
                 stylers: [{ visibility: 'off' }]
             },
             {
                 featureType: 'poi.business',
+                elementType: 'all',
                 stylers: [{ visibility: 'off' }]
             },
             {
                 featureType: 'poi.place_of_worship',
+                elementType: 'all',
                 stylers: [{ visibility: 'off' }]
             },
             {
                 featureType: 'poi.school',
+                elementType: 'all',
                 stylers: [{ visibility: 'off' }]
             },
             {
                 featureType: 'poi.medical',
+                elementType: 'all',
                 stylers: [{ visibility: 'off' }]
             },
             {
                 featureType: 'poi.park',
+                elementType: 'all',
                 stylers: [{ visibility: 'off' }]
             },
             {
                 featureType: 'poi.attraction',
+                elementType: 'all',
                 stylers: [{ visibility: 'off' }]
             },
             {
                 featureType: 'poi.landmark',
+                elementType: 'all',
                 stylers: [{ visibility: 'off' }]
             },
             {
                 featureType: 'poi.point_of_interest',
+                elementType: 'all',
                 stylers: [{ visibility: 'off' }]
             }
         ]
